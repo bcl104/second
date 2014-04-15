3b98a15cf31e8cca6c1160ae60f68c957ddf4ea7
bcl1104 <bcl1104@163.com>
2014-04-15 10:59:04 +0800

    four

--- src/share.cpp
+++ src/share.cpp
@@ -12,6 +12,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout<<"fjeigj"<<endl;
-	cout<<"grioehgr"<<end;
+	cout<<"grioehgr"<<endl;
 	return 0;
 }
